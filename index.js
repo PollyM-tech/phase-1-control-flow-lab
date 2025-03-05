b1562c19d6528577a11a8c5a7549941e23e7dd4c
Pauline Moraa <pmoraa431@gmail.com>
2025-03-06 02:57:52 +0300

    Control Flow

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+ let greeting;
+
+ if (ride <= 400){
+  greeting = 'This one is on me!'
+ }else if(ride>400 && ride <= 2000){
+  greeting = 'That will be twenty bucks.'
+}else if(ride>2000 && ride <=2500){
+ greeting = 'I will gladly take your thirty bucks.'
+}
+else if(ride >2500){
+  greeting = 'No can do.'
+}
+return greeting;
+
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(NYC){
   // Write your code here!
+const greeting =NYC === 'NYC'? ("Ok, sounds good.") : ("No go.")
+
+return greeting;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+      case 'not as generous':
+        return 'Thank you.';
+        break;
+    default:
+      return 'Bye.';
+      break;
+  }
 }
